197cd3faba56f2febb2e5641120e884e88eebb79
Ivan Chen <IvanChen07@gmail.com>
2022-06-23 15:55:19 -0400

    Initial commit

---
+++ src/main/java/com/stuypulse/robot/constants/Motors.java
@@ -0,0 +1,17 @@
+/************************ PROJECT DORCAS ************************/
+/* Copyright (c) 2022 StuyPulse Robotics. All rights reserved.  */
+/* This work is licensed under the terms of the MIT license.    */
+/****************************************************************/
+
+package com.stuypulse.robot.constants;
+
+/*-
+ * File containing all of the configurations that different motors require.
+ *
+ * Such configurations include:
+ *  - If it is Inverted
+ *  - The Idle Mode of the Motor
+ *  - The Current Limit
+ *  - The Open Loop Ramp Rate
+ */
+public final class Motors {}
---
+++ src/main/java/com/stuypulse/robot/constants/Ports.java
@@ -0,0 +1,15 @@
+/************************ PROJECT DORCAS ************************/
+/* Copyright (c) 2022 StuyPulse Robotics. All rights reserved.  */
+/* This work is licensed under the terms of the MIT license.    */
+/****************************************************************/
+
+package com.stuypulse.robot.constants;
+
+/** This file contains the different ports of motors, solenoids and sensors */
+public final class Ports {
+    public interface Gamepad {
+        int DRIVER = 0;
+        int OPERATOR = 1;
+        int DEBUGGER = 2;
+    }
+}
