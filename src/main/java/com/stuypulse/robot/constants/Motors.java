e2e571991eed00c90c86a0d87dcb4ab2501be0ab
anivanchen <ivanchen07@gmail.com>
2022-06-24 00:48:48 -0400

    Add classes to store motor configuration

--- src/main/java/com/stuypulse/robot/constants/Motors.java
+++ src/main/java/com/stuypulse/robot/constants/Motors.java
@@ -23,4 +23,76 @@ public interface Motors {
     
     public TalonSRXConfig CLIMBER = new TalonSRXConfig(true, NeutralMode.Brake, 80, 1.0 / 5.0);
     
+    /** Classes to store all of the values a motor needs */
+
+    public static class TalonSRXConfig {
+        public final boolean INVERTED;
+        public final NeutralMode NEUTRAL_MODE;
+        public final int PEAK_CURRENT_LIMIT_AMPS;
+        public final double OPEN_LOOP_RAMP_RATE;
+
+        public TalonSRXConfig(
+            boolean inverted,
+            NeutralMode neutralMode,
+            int peakCurrentLimitAmps,
+            double openLoopRampRate
+        ) {
+            this.INVERTED = inverted;
+            this.NEUTRAL_MODE = neutralMode;
+            this.PEAK_CURRENT_LIMIT_AMPS = peakCurrentLimitAmps;
+            this.OPEN_LOOP_RAMP_RATE = openLoopRampRate;
+        }
+
+        public TalonSRXConfig(boolean inverted, NeutralMode neutralMode, int peakCurrentLimitAmps) {
+            this(inverted, neutralMode, peakCurrentLimitAmps, 0.0);
+        }
+
+        public TalonSRXConfig(boolean inverted, NeutralMode neutralMode) {
+            this(inverted, neutralMode, 80);
+        }
+
+        public void configure(WPI_TalonSRX motor) {
+            motor.setInverted(INVERTED);
+            motor.setNeutralMode(NEUTRAL_MODE);
+            motor.configContinuousCurrentLimit(PEAK_CURRENT_LIMIT_AMPS - 10, 0);
+            motor.configPeakCurrentLimit(PEAK_CURRENT_LIMIT_AMPS, 0);
+            motor.configPeakCurrentDuration(100, 0);
+            motor.enableCurrentLimit(true);
+            motor.configOpenloopRamp(OPEN_LOOP_RAMP_RATE);
+        }
+    }
+
+    public static class SparkMaxConfig {
+        public final boolean INVERTED;
+        public final IdleMode IDLE_MODE;
+        public final int CURRENT_LIMIT_AMPS;
+        public final double OPEN_LOOP_RAMP_RATE;
+
+        public SparkMaxConfig(
+                boolean inverted,
+                IdleMode idleMode,
+                int currentLimitAmps,
+                double openLoopRampRate) {
+            this.INVERTED = inverted;
+            this.IDLE_MODE = idleMode;
+            this.CURRENT_LIMIT_AMPS = currentLimitAmps;
+            this.OPEN_LOOP_RAMP_RATE = openLoopRampRate;
+        }
+
+        public SparkMaxConfig(boolean inverted, IdleMode idleMode, int currentLimitAmps) {
+            this(inverted, idleMode, currentLimitAmps, 0.0);
+        }
+
+        public SparkMaxConfig(boolean inverted, IdleMode idleMode) {
+            this(inverted, idleMode, 80);
+        }
+
+        public void configure(CANSparkMax motor) {
+            motor.setInverted(INVERTED);
+            motor.setIdleMode(IDLE_MODE);
+            motor.setSmartCurrentLimit(CURRENT_LIMIT_AMPS);
+            motor.setOpenLoopRampRate(OPEN_LOOP_RAMP_RATE);
+            motor.burnFlash();
+        }
+    }
 }
