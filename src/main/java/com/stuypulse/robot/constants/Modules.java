de289b2f480ba48789cc677c2e0a6833e235c12c
Myles Pasetsky <mp3blue99@gmail.com>
2022-07-09 00:38:02 -0400

    Instantiate modules
    Co-authored-by: ShauryaSen <ShauryaSen@users.noreply.github.com>
    Co-authored-by: BenG49 <BenG49@users.noreply.github.com>
    Co-authored-by: Ivan Chen <IvanChen07@gmail.com>
    Co-authored-by: TraceyLin <TraceyLin@users.noreply.github.com>
    Co-authored-by: Samuel Chen <SamChen1738@users.noreply.github.com>
    Co-authored-by: Zixi Feng <zixifeng12@users.noreply.github.com>
    Co-authored-by: vincentw <vinowang921@gmail.com>

---
+++ src/main/java/com/stuypulse/robot/constants/Modules.java
@@ -0,0 +1,60 @@
+package com.stuypulse.robot.constants;
+
+
+import com.stuypulse.robot.subsystems.swerve.DriveControl;
+import com.stuypulse.robot.subsystems.swerve.Module;
+import com.stuypulse.robot.subsystems.swerve.TurnControl;
+import com.stuypulse.stuylib.math.Angle;
+import com.stuypulse.stuylib.network.SmartAngle;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+import static com.stuypulse.robot.constants.Ports.Swerve.*;
+
+public interface Modules {
+    Module[] MODULES = {
+        new Module(
+            "Swerve/Front Right", 
+            new Translation2d(+Settings.Swerve.SIZE.getX() / 2, -Settings.Swerve.SIZE.getY() / 2), 
+            new DriveControl(FrontRight.DRIVE_MOTOR), 
+            new TurnControl(
+                FrontRight.TURN_MOTOR, 
+                FrontRight.ENCODER, 
+                new SmartAngle("Swerve/Front Right/Angle Offset", Angle.fromDegrees(125.0)).useDegrees()
+            )
+        ),
+        
+        new Module(
+            "Swerve/Front Left", 
+            new Translation2d(+Settings.Swerve.SIZE.getX() / 2, +Settings.Swerve.SIZE.getY() / 2), 
+            new DriveControl(FrontLeft.DRIVE_MOTOR),
+            new TurnControl(
+                FrontLeft.TURN_MOTOR, 
+                FrontLeft.ENCODER, 
+                new SmartAngle("Swerve/Front Left/Angle Offset", Angle.fromDegrees(-145.0)).useDegrees()
+            )
+        ),
+        
+        new Module(
+            "Swerve/Back Left", 
+            new Translation2d(-Settings.Swerve.SIZE.getX() / 2, +Settings.Swerve.SIZE.getY() / 2), 
+            new DriveControl(BackLeft.DRIVE_MOTOR),
+            new TurnControl(
+                BackLeft.TURN_MOTOR,
+                BackLeft.ENCODER, 
+                new SmartAngle("Swerve/Back Left/Angle Offset", Angle.fromDegrees(-35.0)).useDegrees()
+            )
+        ),
+
+        new Module(
+            "Swerve/Back Right", 
+            new Translation2d(-Settings.Swerve.SIZE.getX() / 2, -Settings.Swerve.SIZE.getY() / 2), 
+            new DriveControl(BackRight.DRIVE_MOTOR),
+            new TurnControl(
+                BackRight.TURN_MOTOR, 
+                BackRight.ENCODER, 
+                new SmartAngle("Swerve/Back Right/Angle Offset", Angle.fromDegrees(145.0)).useDegrees()
+            )
+        ),
+    };
+}
