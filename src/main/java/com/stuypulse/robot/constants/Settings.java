07b19d5ef5e8742e50899b693cee8f95fcf5a66e
Myles Pasetsky <mp3blue99@gmail.com>
2022-07-09 00:37:33 -0400

    Add more swerve constants and smart numbers
    Co-authored-by: ShauryaSen <ShauryaSen@users.noreply.github.com>
    Co-authored-by: BenG49 <BenG49@users.noreply.github.com>
    Co-authored-by: Ivan Chen <IvanChen07@gmail.com>
    Co-authored-by: TraceyLin <TraceyLin@users.noreply.github.com>
    Co-authored-by: Samuel Chen <SamChen1738@users.noreply.github.com>
    Co-authored-by: Zixi Feng <zixifeng12@users.noreply.github.com>
    Co-authored-by: vincentw <vinowang921@gmail.com>

--- src/main/java/com/stuypulse/robot/constants/Settings.java
+++ src/main/java/com/stuypulse/robot/constants/Settings.java
@@ -8,8 +8,10 @@ package com.stuypulse.robot.constants;
 import com.stuypulse.stuylib.control.Controller;
 import com.stuypulse.stuylib.control.PIDController;
 import com.stuypulse.stuylib.control.angle.AngleController;
+import com.stuypulse.stuylib.network.SmartNumber;
 
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 
 /*-
@@ -19,8 +21,10 @@ import edu.wpi.first.math.util.Units;
  * values that we can edit on Shuffleboard.
  */
 public interface Settings {
-
     public interface Swerve {
+        Translation2d SIZE = new Translation2d(Units.inchesToMeters(29.0), Units.inchesToMeters(29.0));
+        
+        double MAX_SPEED = Units.feetToMeters(14.420); 
 
         public interface DriveControl {
             public interface Encoder {
@@ -32,9 +36,9 @@ public interface Settings {
                 double VELOCITY_CONVERSION = POSITION_CONVERSION / 60.0;
             }
             public interface Feedback {
-                double kP = 0.0;
-                double kI = 0.0;
-                double kD = 0.0;
+                SmartNumber kP = new SmartNumber("Swerve/Drive/P", 1.0);
+                SmartNumber kI = new SmartNumber("Swerve/Drive/I", 0.0);
+                SmartNumber kD = new SmartNumber("Swerve/Drive/D", 0.0);
 
                 public static Controller getFeedback() {
                     return new PIDController(kP, kI, kD);
@@ -42,9 +46,9 @@ public interface Settings {
             }
 
             public interface FeedForward {
-                double kS = 0.0;
-                double kV = 0.0;
-                double kA = 0.0;
+                double kS = 0.11004;
+                double kV = 2.7859;
+                double kA = 0.10702;
 
                 public static SimpleMotorFeedforward getFeedForward() {
                     return new SimpleMotorFeedforward(kS, kV, kA);
@@ -62,17 +66,17 @@ public interface Settings {
                 double MAX_OUTPUT = +Math.PI;
 
                 static double getRadians(double input) {
-                    return ((input - MIN_INPUT) / MIN_OUTPUT) * (MAX_OUTPUT - MIN_OUTPUT) + MIN_OUTPUT;
-                }                
+                    return ((input - MIN_INPUT) / (MAX_INPUT - MIN_INPUT)) * (MAX_OUTPUT - MIN_OUTPUT) + MIN_OUTPUT;
+                }
             }
 
             public interface Feedback {
-                double kP = 0.0;
-                double kI = 0.0;
-                double kD = 0.0;
+                SmartNumber kP = new SmartNumber("Swerve/Turn/P", 2.0);
+                SmartNumber kI = new SmartNumber("Swerve/Turn/I", 0.0);
+                SmartNumber kD = new SmartNumber("Swerve/Turn/D", 0.0);
 
                 public static AngleController getFeedback() {
-                    return new PIDController(kP, kI, kD).angle();
+                    return new PIDController(kP, kI, kD).angle().useRadians();
                 }
             }
         }
