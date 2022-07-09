d1351371775ba23a054c19bcb8226cb6b6b0bcb5
Myles Pasetsky <mp3blue99@gmail.com>
2022-07-09 00:39:30 -0400

    Add setSpeeds, kinematics, odometry to subsystem
    Co-authored-by: ShauryaSen <ShauryaSen@users.noreply.github.com>
    Co-authored-by: BenG49 <BenG49@users.noreply.github.com>
    Co-authored-by: Ivan Chen <IvanChen07@gmail.com>
    Co-authored-by: TraceyLin <TraceyLin@users.noreply.github.com>
    Co-authored-by: Samuel Chen <SamChen1738@users.noreply.github.com>
    Co-authored-by: Zixi Feng <zixifeng12@users.noreply.github.com>
    Co-authored-by: vincentw <vinowang921@gmail.com>

--- src/main/java/com/stuypulse/robot/subsystems/Swerve.java
+++ src/main/java/com/stuypulse/robot/subsystems/Swerve.java
@@ -1,10 +1,23 @@
 package com.stuypulse.robot.subsystems;
 
-import com.stuypulse.robot.subsystems.swerve.DriveControl;
-import com.stuypulse.robot.subsystems.swerve.TurnControl;
+import java.util.Arrays;
+
+import com.kauailabs.navx.frc.AHRS;
+import com.stuypulse.robot.constants.Modules;
+import com.stuypulse.stuylib.math.Vector2D;
 import com.stuypulse.robot.subsystems.swerve.Module;
 
+import static com.stuypulse.robot.constants.Settings.Swerve.*;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
@@ -18,8 +31,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
  * Fields:
  *  - 4 Modules
  *  - navX
- *  - Kinematics
- *  - Odometry
+ *  - SwerveDriveKinematics
+ *  - SwerveDriveOdometry
  * 
  * @author Myles Pasetsky (myles.pasetsky@gmail.com)
  * @author Vincent Wang (vincent@goldfisher.com)
@@ -30,7 +43,98 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 */
 public class Swerve extends SubsystemBase {
     
-	private Module[] modules;
+	private final Module[] modules;
+	private final AHRS gyro;
 
-	public Swerve() {}
+    private final SwerveDriveKinematics kinematics;
+	private final SwerveDriveOdometry odometry;
+
+	public Swerve() {
+        modules = Modules.MODULES;		
+        gyro = new AHRS(SPI.Port.kMXP);
+		kinematics =  new SwerveDriveKinematics(
+            Arrays.stream(modules)
+                .map(x -> x.getLocation())
+                .toArray(Translation2d[]::new)
+        );
+        odometry = new SwerveDriveOdometry(kinematics, getGyroAngle());
+	}
+
+    /** GYROSCOPE */
+
+    public Rotation2d getGyroAngle() {
+		return gyro.getRotation2d();
+    }
+
+    /** ODOMETRY */
+
+    public Pose2d getPose() {
+		return odometry.getPoseMeters();
+    }
+
+    public Rotation2d getAngle() {
+        return getPose().getRotation();
+    }
+
+    public Translation2d getTranslation() {
+        return getPose().getTranslation();
+    }
+
+    public void reset(Pose2d pose)  {
+		odometry.resetPosition(pose, getGyroAngle());
+    }
+
+    private void update() {
+        odometry.update(getGyroAngle(), getModuleStates());
+    }
+
+    /** MODULES */
+
+    private SwerveModuleState[] getModuleStates() {
+        // Arrays.stream(modules).map(x -> x.getState()).toArray(SwerveModuleState[]::new);
+        SwerveModuleState[] states = new SwerveModuleState[modules.length];
+        
+        for (int i = 0; i < modules.length; i++) {
+            states[i] = modules[i].getState();
+        }
+        
+        return states;
+    }
+
+    public void setSpeeds(Vector2D velocity, double angVel, boolean fieldRelative) {
+        if (fieldRelative) {
+            ChassisSpeeds chassisSpeed = ChassisSpeeds.fromFieldRelativeSpeeds(velocity.y, -velocity.x, angVel, getAngle());
+            setSpeeds(chassisSpeed);
+        } else {
+            ChassisSpeeds chassisSpeed = new ChassisSpeeds(velocity.y, -velocity.x, angVel);
+		    setSpeeds(chassisSpeed);
+        }
+    }
+
+	public void setSpeeds(ChassisSpeeds chassisSpeed) {
+        setSpeeds(kinematics.toSwerveModuleStates(chassisSpeed));   
+	}
+	
+	public void setSpeeds(SwerveModuleState[] states) {
+        if (states.length != modules.length) {
+			throw new IllegalArgumentException("Number of desired module states does not match the number of modules. ");
+        }
+		
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_SPEED);
+
+		for (int i = 0; i < states.length; i++) {
+			modules[i].setTargetState(states[i]);
+		}
+	}
+	
+    @Override
+    public void periodic() {
+        update();
+        
+        SmartDashboard.putNumber("Swerve/Pose X", getTranslation().getX());
+        SmartDashboard.putNumber("Swerve/Pose Y", getTranslation().getY());
+        SmartDashboard.putNumber("Swerve/Pose Angle", getAngle().getDegrees());
+        SmartDashboard.putNumber("Swerve/Gyro Angle", getAngle().getDegrees());
+    }
+
 }
