c9af6dd8b94632d68766a82c92305716645409fd
Vincent Wang <vinowang921@gmail.com>
2022-07-24 13:57:32 -0800

    Add Intake Commands

---
+++ src/main/java/com/stuypulse/robot/commands/intake/IntakeDeacquire.java
@@ -0,0 +1,34 @@
+package com.stuypulse.robot.commands.intake;
+
+import com.stuypulse.robot.subsystems.Intake;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class IntakeDeacquire extends CommandBase {
+
+    private final Intake intake;
+
+    public IntakeDeacquire(Intake intake) {
+        this.intake = intake;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize() {
+        intake.deacquire();
+    }
+
+    @Override
+    public void execute() {
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        intake.stop();
+    }
+}
