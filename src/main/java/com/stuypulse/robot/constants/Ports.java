93fdf10bddfbc3e866d29e24fb00efd30c5dc557
anivanchen <ivanchen07@gmail.com>
2022-07-27 20:43:15 -0400

    Revert "Revert "Merge remote-tracking branch 'origin/main' into se/climber""
    This reverts commit af9c56c16611a2a0818a5ccb7bfa30b24c4f97d6.

--- src/main/java/com/stuypulse/robot/constants/Ports.java
+++ src/main/java/com/stuypulse/robot/constants/Ports.java
@@ -7,6 +7,7 @@ package com.stuypulse.robot.constants;
 
 /** This file contains the different ports of motors, solenoids and sensors */
 public interface Ports {
+
     public interface Gamepad {
         int DRIVER = 0;
         int OPERATOR = 1;
