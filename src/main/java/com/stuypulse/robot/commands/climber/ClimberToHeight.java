4d8af1eb04d7699d2d7b22305502a84f0d6733b0
Eric Lin <ericlin071906@gmail.com>
2022-09-28 16:27:50 -0400

    Rename ClimberToHeight

--- src/main/java/com/stuypulse/robot/commands/climber/ClimberToHeight.java
+++ src/main/java/com/stuypulse/robot/commands/climber/ClimberToHeight.java
@@ -5,20 +5,20 @@ import com.stuypulse.robot.subsystems.Climber;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
-public class ClimberMoveToHeight extends CommandBase{
+public class ClimberToHeight extends CommandBase{
     
     private final Climber climber;
     private final double height; 
     private boolean instant;
 
-    public ClimberMoveToHeight(Climber climber, double height){
+    public ClimberToHeight(Climber climber, double height){
         this.climber = climber;
         this.height = height;
         instant = true;
         addRequirements(climber);
     }
     
-    public final ClimberMoveToHeight untilReady(){
+    public final ClimberToHeight untilReady(){
         instant = false;
         return this;
     }
