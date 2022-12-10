535f32a0e8cec5bada6939cff4ffa1ad791d0035
Myles Pasetsky <mp3blue99@gmail.com>
2022-12-09 20:43:54 -0500

    add kind of sensible gamepad bindings

--- src/main/java/com/stuypulse/robot/RobotContainer.java
+++ src/main/java/com/stuypulse/robot/RobotContainer.java
@@ -7,14 +7,19 @@ package com.stuypulse.robot;
 
 import com.stuypulse.robot.commands.auton.DoNothingAuton;
 import com.stuypulse.robot.commands.climber.*;
+import com.stuypulse.robot.commands.conveyor.ConveyorSetMode;
 import com.stuypulse.robot.commands.intake.*;
+import com.stuypulse.robot.commands.shooter.ShooterSetRPM;
 import com.stuypulse.robot.constants.Ports;
 import com.stuypulse.robot.subsystems.*;
 import com.stuypulse.robot.subsystems.shooter.*;
+import com.stuypulse.robot.util.ConveyorMode;
 import com.stuypulse.robot.subsystems.intake.*;
 import com.stuypulse.robot.subsystems.climber.*;
 import com.stuypulse.stuylib.input.Gamepad;
 import com.stuypulse.stuylib.input.gamepads.Xbox;
+import com.stuypulse.stuylib.network.SmartNumber;
+
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -26,9 +31,9 @@ public class RobotContainer {
 
   // Subsystem
   public final Conveyor conveyor = new Conveyor();
-  public final IShooter shooter = new SimShooter();
-  public final IIntake intake = new SimIntake();
-  public final IClimber climber = new SimClimber();
+  public final IShooter shooter = new Shooter();
+  public final IIntake intake = new Intake();
+  public final IClimber climber = new Climber();
   public final Swerve swerve = new Swerve();
 
   // Gamepads
@@ -58,27 +63,27 @@ public class RobotContainer {
   /***************/
 
   private void configureButtonBindings() {
-    driver.getRightTriggerButton()
-      .whenPressed(new IntakeExtend(intake))
-      .whileHeld(new IntakeAcquire(intake))
-      .whenReleased(new IntakeRetract(intake));
-
-    driver.getLeftTriggerButton()
-      .whileHeld(new IntakeDeacquire(intake));
 
     operator.getLeftTriggerButton()
-      .whenHeld(new IntakeRetract(intake));
+      .whileHeld(new IntakeDeacquire(intake))
+      .whileHeld(new ConveyorSetMode(conveyor, ConveyorMode.REVERSE));
 
     operator.getRightTriggerButton()
-      .whenHeld(new IntakeAcquire(intake))
+      .whileHeld(new IntakeAcquire(intake))
       .whenPressed(new IntakeExtend(intake))
       .whenReleased(new IntakeRetract(intake));
 
-    operator.getTopButton()
-      .whenPressed(new ClimberToTop(climber));
-
     operator.getBottomButton()
-      .whenPressed(new ClimberToBottom(climber));
+      .whenPressed(new ConveyorSetMode(conveyor, ConveyorMode.INDEXING));  
+    operator.getRightButton()
+      .whenPressed(new ConveyorSetMode(conveyor, ConveyorMode.SHOOTING));
+    operator.getLeftButton()
+      .whenPressed(new ConveyorSetMode(conveyor, ConveyorMode.BRING_UP_BALLS));
+
+    operator.getDPadDown().whenPressed(new ShooterSetRPM(shooter, 0));
+    operator.getDPadLeft().whenPressed(new ShooterSetRPM(shooter, 1100));
+    operator.getDPadRight().whenPressed(new ShooterSetRPM(shooter, 3000));
+    operator.getDPadUp().whenPressed(new ShooterSetRPM(shooter, new SmartNumber("Shooter/Tuned", 2000)));
   }
 
   /**************/
