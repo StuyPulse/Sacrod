da380fc3148d0174595437ab37dc4050890a9ba0
ambers7 <ambrshen@gmail.com>
2023-10-12 14:36:51 -0400

    Finish balance

--- src/main/java/com/stuypulse/robot/commands/auton/OnePieceDock.java
+++ src/main/java/com/stuypulse/robot/commands/auton/OnePieceDock.java
@@ -1,7 +1,30 @@
 package com.stuypulse.robot.commands.auton;
 
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.stuypulse.robot.RobotContainer;
+import com.stuypulse.robot.commands.shooter.ShootHigh;
+import com.stuypulse.robot.commands.swerve.FollowTrajectory;
+import com.stuypulse.robot.commands.swerve.SwerveDriveBalance;
+import com.stuypulse.robot.constants.Settings.Swerve.Motion;
+
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class OnePieceDock extends SequentialCommandGroup{
-    
+    public OnePieceDock(RobotContainer robot, String path) {
+
+        // Time it takes for the shooter to reach the target speed
+        double SHOOTER_INITIALIZE_DELAY = 0.3;
+
+        PathPlannerTrajectory traj = PathPlanner.loadPath(path, Motion.CONSTRAINTS);
+        
+        addCommands(
+                new ShootHigh(),
+                new WaitCommand(SHOOTER_INITIALIZE_DELAY),
+
+                new FollowTrajectory(traj)
+                        .robotRelative(),
+                new SwerveDriveBalance());
+}
 }
