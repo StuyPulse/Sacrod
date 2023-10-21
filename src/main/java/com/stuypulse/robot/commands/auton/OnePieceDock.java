ec5cf0a5c1ec9aa6335e1cb96e2964b993961795
Ivan Chen <ivanchen07@gmail.com>
2023-10-21 11:31:31 -0400

    Tune one piece mob nw and two piece dock w autons

--- src/main/java/com/stuypulse/robot/commands/auton/OnePieceDock.java
+++ src/main/java/com/stuypulse/robot/commands/auton/OnePieceDock.java
@@ -9,7 +9,6 @@ import com.stuypulse.robot.commands.shooter.ShooterStop;
 import com.stuypulse.robot.commands.swerve.FollowTrajectory;
 import com.stuypulse.robot.commands.swerve.SwerveDriveBalance;
 import com.stuypulse.robot.commands.swerve.SwerveDriveResetHeading;
-import com.stuypulse.robot.constants.Settings.Swerve.Motion;
 import com.stuypulse.robot.util.ConveyorMode;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -22,7 +21,8 @@ public class OnePieceDock extends SequentialCommandGroup {
         // Time it takes for the shooter to reach the target speed
         double SHOOTER_INITIALIZE_DELAY = 0.5;
 
-        PathPlannerTrajectory traj = PathPlanner.loadPath(path, Motion.CONSTRAINTS);
+        // PathPlannerTrajectory traj = PathPlanner.loadPath(path, Motion.CONSTRAINTS);
+        PathPlannerTrajectory traj = PathPlanner.loadPath(path, new PathConstraints(4, 3));
         
         addCommands(
                 new SwerveDriveResetHeading(),
--- src/main/java/com/stuypulse/robot/commands/auton/OnePieceMobilityDock.java
+++ src/main/java/com/stuypulse/robot/commands/auton/OnePieceMobilityDock.java
@@ -8,6 +8,7 @@ import com.stuypulse.robot.commands.shooter.ShootHigh;
 import com.stuypulse.robot.commands.shooter.ShooterStop;
 import com.stuypulse.robot.commands.swerve.FollowTrajectory;
 import com.stuypulse.robot.commands.swerve.SwerveDriveBalance;
+import com.stuypulse.robot.commands.swerve.SwerveDriveResetHeading;
 import com.stuypulse.robot.constants.Settings.Swerve.Motion;
 import com.stuypulse.robot.util.ConveyorMode;
 
@@ -21,15 +22,18 @@ public class OnePieceMobilityDock extends SequentialCommandGroup {
         // Time it takes for the shooter to reach the target speed
         double SHOOTER_INITIALIZE_DELAY = 0.5;
 
-        PathPlannerTrajectory traj = PathPlanner.loadPath(path, Motion.CONSTRAINTS);
+        // PathPlannerTrajectory traj = PathPlanner.loadPath(path, Motion.CONSTRAINTS);
+        PathPlannerTrajectory traj = PathPlanner.loadPath(path, new PathConstraints(4, 3));
         PathPlannerTrajectory csTraj = PathPlanner.loadPath(csPath, new PathConstraints(1, 1));
-        PathPlannerTrajectory upCSTraj = PathPlanner.loadPath(upCSPath, Motion.CONSTRAINTS);
+        // PathPlannerTrajectory upCSTraj = PathPlanner.loadPath(upCSPath, Motion.CONSTRAINTS);
+        PathPlannerTrajectory upCSTraj = PathPlanner.loadPath(upCSPath, new PathConstraints(4, 3));
         
         // shoot cube
         addCommands(
+                new SwerveDriveResetHeading(),
                 new ShootHigh(),
                 new WaitCommand(SHOOTER_INITIALIZE_DELAY),
-                new ConveyorSetMode(ConveyorMode.SHOOTING).withTimeout(SHOOTER_INITIALIZE_DELAY),
+                new ConveyorSetMode(ConveyorMode.FORWARD).withTimeout(SHOOTER_INITIALIZE_DELAY),
                 new ShooterStop()
         );
 
